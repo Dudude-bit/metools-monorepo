53e3d2651656deb04821e18426dc1f9423ea9a3a
kirillinakin <kirillinyakin@yandex.ru>
2024-02-25 17:58:28 +0300

    Migrations init

--- metools-backend/migrations/2024-02-24-232903_create_users/up.sql
+++
@@ -1,8 +0,0 @@
--- Your SQL goes here
-CREATE TABLE `users`(
-	`id` UUID NOT NULL PRIMARY KEY,
-	`title` VARCHAR NOT NULL,
-	`body` TEXT NOT NULL,
-	`published` BOOL NOT NULL
-);
-
--- metools-backend/migrations/2024-02-25-014218_add_rzd_tasks_table/up.sql
+++ metools-backend/migrations/2024-02-25-014218_add_rzd_tasks_table/up.sql
@@ -2,5 +2,6 @@
 CREATE TABLE rzd_tasks(
     id uuid not null primary key ,
     type text not null ,
-    data jsonb not null
+    data jsonb not null,
+    user_id uuid not null references users(id)
 )
